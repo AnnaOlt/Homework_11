5cbf91d98ccbfe4e718ec307f9deebc83d272432
Anna Olt <anna.olt16@gmail.com>
2020-03-20 17:39:51 -0400

    all backend routes and methods set up

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -9,23 +9,53 @@ module.exports = function(app) {
     fs.readFile(filePath, "utf8", function(err, data) {
       if (err) throw err;
       console.log("Got data back");
+      // Return all data as a JSON instead of an array
       console.log(data);
+      res.json({ notes: data });
     });
-    res.json({ Status: "Success" });
   });
-
-  //   app.get("/api/data", function(req, res) {
-  //     res.json(data);
-  //   });
 
   app.post("/api/notes", function(req, res) {
-    tableData.push(req.body);
-    res.json(true);
+    // Check what the body is
+    //console.log(req.body);
+    // Create a note using req.body
+    let note = req.body;
+    // Read from the db.json file
+    fs.readFile(filePath, "utf8", function(err, data) {
+      if (err) throw err;
+      // Return all data as a JSON instead of an array
+      let notes = JSON.parse(data);
+      // Append to the data that was read
+      notes.push(note);
+      // Insert new notes bacl into the db which in our case is just a json file
+      fs.writeFile(filePath, JSON.stringify(notes), function(err, data) {
+        if (err) throw err;
+        res.json(note);
+      });
+    });
+    // Send a response back
+    //notesData.push(req.body);
   });
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    notesData.length = 0;
 
-    res.json({ ok: true });
+  app.delete("/api/notes/:id", function(req, res) {
+    // Extract id and console.log it
+    console.log(req.params.id);
+    let noteId = req.params.id;
+    // // Read from the db.json file
+    fs.readFile(filePath, "utf8", function(err, data) {
+      if (err) throw err;
+      // Return all data as a JSON instead of an array
+      let notes = JSON.parse(data);
+      // Remove the note with the given noteId
+      let newArr = notes.filter(note => {
+        return note._id != noteId;
+      });
+      console.log(newArr);
+      //  Insert new notes back into the db which in our case is just a json file
+      fs.writeFile(filePath, JSON.stringify(newArr), function(err, data) {
+        if (err) throw err;
+        res.json(newArr);
+      });
+    });
   });
 };
--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
 const http = require("http");
+const fs = require("fs");
 
 const app = express();
 
